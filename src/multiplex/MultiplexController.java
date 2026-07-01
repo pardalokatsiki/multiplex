2eceae1c2f64776510c4a6094eeb3ccfc5b15750
convr-dev <konstantina.varvaruc@gmail.com>
2026-07-01 18:50:10 +0300

    Add scene switching functionality for Login and Register pages

--- src/multiplex/MultiplexController.java
+++ src/multiplex/MultiplexController.java
@@ -1,7 +1,13 @@
 package multiplex;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
@@ -10,6 +16,42 @@ import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
 public class MultiplexController {
+    @FXML
+    public void loginButtonClick(ActionEvent login) {
+        String text = userField.getText();
+        String pwd = passField.getText();
+        if(!text.isEmpty() && !pwd.isEmpty())
+            System.out.println("Looking for user: " + text + " with password: " + pwd);
+    }
+    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+   
+    @FXML
+    public void registerLinkClick(ActionEvent event) throws IOException {
+        //Move to Register Page Scene
+        root = FXMLLoader.load(getClass().getResource("CreateUserPageScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void createAccountClick(ActionEvent event) throws IOException {
+        String username = createUsername.getText();
+        String password = createPass.getText();
+        
+        //TODO Add SQL query to save user credentials
+        
+        //Move to Login Page Scene
+        root = FXMLLoader.load(getClass().getResource("LoginPageScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     private Label cinemaLogo;
@@ -30,15 +72,11 @@ public class MultiplexController {
     private TextField userField;
 
     @FXML
-    public void loginButtonClick(ActionEvent event) {
-        String text = userField.getText();
-        String pwd = passField.getText();
-        if(!text.isEmpty() && !pwd.isEmpty())
-            System.out.println("Looking for user: " + text + " with password: " + pwd);
-    }
+    private Button createAccountButton;
 
     @FXML
-    public void registerLinkClick(ActionEvent event) {
-        //Move to Register Page Scene
-    }
+    private TextField createPass;
+
+    @FXML
+    private TextField createUsername;
 }
