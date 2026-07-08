f8d11181fe0fb436a5ee8e3bbc37a44daba44fee
Panagiotis <pkoufolias@gmail.com>
2026-07-08 19:18:51 +0300

    Revert "Merge branch 'staging' of https://github.com/pardalokatsiki/multiplex into staging"
    This reverts commit 3c0a214450d75f7b867aaf6f0125be7740178905, reversing
    changes made to 1404bfede9dc966dfe1bc450726390959430a220.

--- src/multiplex/controllers/LoginController.java
+++ src/multiplex/controllers/LoginController.java
@@ -38,17 +38,16 @@ public class LoginController {
         // Username field shouldn't be empty and at most 18 characters long
         // Password field shouldn't be empty and at least 8 characters long
         if ((!name.isEmpty() && name.length() <= 18)
-                || (!pwd.isEmpty() && pwd.length() >= 8)) {
+                && (!pwd.isEmpty() && pwd.length() >= 8)) {
             // Check if user exists in the database
             UserService check = new UserService();
             // returns user if succesful, null if not
             User user = check.loginUser(name, pwd);
 
-            if (user != null) {
+            if (user != null)
                 // Move to Browse Page Scene
                 Session.setCurrentUser(user);
                 MultiplexController.switchScene(ViewScenes.BROWSE);
-            }
             // user = null
             errorLabel.setText("Login failed. Username With That Password Not Found\n");
         } else {
